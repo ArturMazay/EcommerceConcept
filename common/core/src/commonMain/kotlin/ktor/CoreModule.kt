ce77803b95323f0a7e48a2eb54ecdaa1ea26802e
arturzukauskas <arthur_z@intime.digital>
2022-11-08 12:27:08 +0200

    добавлен в  core модуль модуль кtor , добавленные платформозависимые движки для ктора

---
+++ common/core/src/commonMain/kotlin/ktor/CoreModule.kt
@@ -0,0 +1,1 @@
+package ktorimport org.kodein.di.DIval coreModule = DI.Module("coreModule") {    importAll(        ktorModule    )}
---
+++ common/core/src/commonMain/kotlin/ktor/HttpEngineFactory.kt
@@ -0,0 +1,1 @@
+package ktorimport io.ktor.client.engine.*internal expect class HttpEngineFactory constructor() {    fun createEngine(): HttpClientEngineFactory<HttpClientEngineConfig>}
